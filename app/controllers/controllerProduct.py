2cd4ca56f4432fd05435386f9747f654045a7279
YamilCr <cruzyamil17@gmail.com>
2025-08-15 22:43:54 -0300

    desarrollo con uv

--- app/controllers/controllerProduct.py
+++ app/controllers/controllerProduct.py
@@ -24,7 +24,7 @@ def fetch_all_products(search: str, limit: int = 30):
             fetch_data_items_neomat(search, 5)
         ]
         try:
-            results_easy = fetch_data_layer_items(search, 10)
+            results_easy = await fetch_data_layer_items(search, 10)
         except Exception as e:
             print(f"[Easy] Error al obtener productos: {e}")
             results_easy = []
@@ -36,15 +36,19 @@ def fetch_all_products(search: str, limit: int = 30):
 
     return asyncio.run(gather_all())
 
+# funcion para la busqueda por proveedor en especifico
 def fetch_products_proveedor(proveedor: str, search: str, limit: int = 20):
     search = search.lower()
     proveedor = proveedor.lower()
 
+    # esto devuelve el metodo scrapper de cada proveedor
     scraper = PROVEEDORES.get(proveedor)
     if not scraper:
         raise RuntimeError(f"Proveedor '{proveedor}' no soportado")
 
+    # esto es para comprobar si es un metodo async o sync del scraper
     if asyncio.iscoroutinefunction(scraper):
+        # realiza la busqueda en el proveedor en especifico
         return asyncio.run(scraper(search, limit))
     else:
         return scraper(search, limit)
